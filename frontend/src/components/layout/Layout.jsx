94d09c5eae0dccf2fa97b764014cbf9dece65ed2
ACER <huyhoang432002@gmail.com>
2023-10-30 20:45:54 +0700

    add footer

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../header/Header'
 import Routers from '../../router/Routers'
+import Footer from '../Footer/Footer'
 
 const Layout = () => {
     return (
         <>
             <Header />
             <Routers/>
+            <Footer></Footer>
         </>
     )
 }
